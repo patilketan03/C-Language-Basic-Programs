37c91751bcd4e30c13d0de102366864dae25632c
patilketan03 <patilketan080@gamil.com>
2022-03-23 09:39:19 +0530

    ADDED:[KETAN] Multiplication of Two Numbers

---
+++ MULTIPLI.C
@@ -0,0 +1,13 @@
+/*Programe to Find Multitiplication of Two Numbers*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+        clrscr();
+        printf("Enter any Two Numbers:");
+        scanf("%d %d",&a,&b);
+        c=a*b;
+        printf("Multiplication=%d",c);
+        getch();
+}
