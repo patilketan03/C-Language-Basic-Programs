322927b29496ae0a8739db68d7a09978934ef9c6
patilketan03 <patilketan080@gamil.com>
2022-03-24 08:52:44 +0530

    ADDED:[KETAN]SUbtraction of Two Numbers

---
+++ SUB.C
@@ -0,0 +1,13 @@
+/*Program to find Subtraction of Two Numbers*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+        clrscr();
+        printf("\nEnter any 2 Numbers:");
+        scanf("%d %d",&a,&b);
+        c=a-b;
+        printf("\nSubtraction=%d",c);
+        getch();
+}
