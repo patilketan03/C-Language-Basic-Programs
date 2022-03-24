f44628f53c460bcc8462a357f6058bdba70e89cf
patilketan03 <patilketan080@gamil.com>
2022-03-24 08:44:46 +0530

    ADDED:[KETAN]Division of Two Numbers

---
+++ DIV.C
@@ -0,0 +1,13 @@
+/*Program to Find Division of Two Number*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c=0;
+        clrscr();
+        printf("\nEnter any 2 Numbers:");
+        scanf("%d %d",&a,&b);
+        c=a/b;
+        printf("\nDivision=%d",c);
+        getch();
+}
