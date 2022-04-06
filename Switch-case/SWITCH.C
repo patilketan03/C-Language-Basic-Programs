1754fcd907dd889443ac0cc8fbc3a39da3104aa2
patilketan03 <patilketan080@gamil.com>
2022-04-06 09:04:12 +0530

    ADDED:[KETAN]Find day using switch case

---
+++ Switch-case/SWITCH.C
@@ -0,0 +1,58 @@
+//Find day Using switch case
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int day;
+        clrscr();
+        printf("Enter day number:>>\n");
+        scanf("%d",&day);
+        switch(day)
+        {
+           case 1:
+           {
+           	printf("Monday");
+                break;
+           }
+           case 2:
+           {
+           	printf("Tuesday");
+           	break;
+           }
+           case 3:
+           {
+
+           	printf("Wednesday");
+           	break;
+           }
+           case 4:
+           {
+           	printf("Thursday");
+                break;
+           }
+           case 5:
+           {
+           	printf("Friday");
+                break;
+           }
+           case 6:
+           {
+           	printf("Saturday");
+                break;
+           }
+           case 7:
+           {
+           	printf("Sunday");
+                break;
+           }
+           default:
+           {
+           	printf("Invalid day number");
+
+           }
+        }
+
+	  printf("\nHave a nice day");
+       	  getch();
+}
+
